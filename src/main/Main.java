89bb32a9ef5f9dfcf3c9b1fb7b128bb558a0ee2c
Pedro Augusto <pedro.study314@gmail.com>
2025-04-09 21:21:02 -0300

    Início

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+import teste_a.Teste_a;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Teste");
+		Teste_a.teste_a();
+
+	}
+
+}
---
+++ src/teste_a/Teste_a.java
@@ -0,0 +1,7 @@
+package teste_a;
+
+public class Teste_a {
+	public static void teste_a() {
+		System.out.println("Teste_aaaaaaa");
+	}
+}
